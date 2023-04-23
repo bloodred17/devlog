c9937050708db68e8cb7ef04887f939fbc7151e5
Ankur Dutta <ankur.611@gmail.com>
2023-04-23 13:43:36 +0530

    Tailwind config added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,24 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("daisyui")
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#EA6A62',
+          secondary: '#F9A592',
+          accent: '#DA3D51',
+          neutral: '#260B0E',
+          "neutral-content": '#FCF9F7',
+          "base-100": '#FCF9F7',
+          "base-200": '#FCF6F2',
+          "base-300": '#FCEEE6',
+        }
+      }
+    ]
+  }
 };
